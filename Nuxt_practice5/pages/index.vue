a991869ed400015660a04fdcf4d0e7d9ef33d67e
向山　翔 <hms0175km@gmail.com>
2021-11-27 14:44:51 +0900

    [finish]　Nuxt.js  axsionのインストールスタイルタグの使い方の学習

---
+++ Nuxt_practice5/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <div class="header">
+      <h1>{{ message }}</h1>
+    </div>
+    <hr>
+  </section>
+</template>
+ 
+  
+<script>
+export default {
+  data: function(){
+    return {
+      message: 'Topページです'
+    }
+  }
+}
+</script>
+<style>
+  h1 {
+    color: red;
+  }
+</style>
